e98ee011925552725edfae3101bac03b6543e491
noeleon <noe@thalheim.email>
2024-06-03 20:17:38 +0200

    feat(ios): link dynamically
    This is needed to use the SQLDelight library

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -23,7 +23,7 @@ kotlin {
     ).forEach { iosTarget ->
         iosTarget.binaries.framework {
             baseName = "Shared"
-            isStatic = true
+            isStatic = false
         }
     }
     
